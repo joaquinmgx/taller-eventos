0602d41edd2b2d0a1855680f95d39302dea05348
Joaquinmgx <141180522+Joaquinmgx@users.noreply.github.com>
2023-08-11 17:10:16 -0300

    div ok

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
 function Saludo() {
     alert('Hola!');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    var boton = document.getElementById('boton');
+    boton.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
